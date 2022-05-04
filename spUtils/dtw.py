735c21d8632d67236e85028ddb7c8568d7fbeabb
pfsun <pfsun@pfsun-m-kfm6.attlocal.net>
2022-05-03 20:12:33 -0700

    add dynamic time warping

---
+++ spUtils/dtw.py
@@ -0,0 +1,10 @@
+# this class provide a general dynamic time wrapping strategy
+
+class dynamicTimeWarpping():
+      def __init__(self):
+          """
+            Dynamic time warpping used to alignment the signals in the time domain 
+          """
+          pass
+
+      def dtw_base()
